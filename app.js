4fa52b62f26b90ecf7380df52d0ac1ba8e0b747a
Ketan222 <ketanmodgil@gmail.com>
2025-07-29 21:50:13 +0530

    feat: created api

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { func } from "./controller/controller.js";
+import { func } from "./controller.js";
 
 const app = new express();
 
 app.use(express.json());
 
 app.post("/api/bfhl", func);
-app.get("/api/chk", func);
+// app.get("/api/chk", func);
 export { app };
